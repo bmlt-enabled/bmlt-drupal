2591cb88eac44f567ba2ff5e81e9ca1645d371f5
Chris M <webservant@magnaws.org>
2011-08-11 18:04:24 -0400

    2.1.20 release.
    This adds the "bmlt_map" short code, which implements a powerful new search feature.

--- bmlt-drupal-satellite-plugin.php
+++ bmlt-drupal-satellite-plugin.php
@@ -3,7 +3,7 @@
 *   \file   bmlt-drupal-satellite-plugin.php                                                *
 *                                                                                           *
 *   \brief  This is a Drupal plugin of a BMLT satellite client.                             *
-*   \version 2.1.19                                                                          *
+*   \version 2.1.20                                                                          *
 *                                                                                           *
     This file is part of the Basic Meeting List Toolbox (BMLT).
     
