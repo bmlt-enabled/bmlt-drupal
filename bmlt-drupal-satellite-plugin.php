13dbed5db27d1e021a855d614259feb3de983613
Chris M <webservant@magnaws.org>
2011-08-12 16:19:41 -0400

    2.1.21 Release 	Fixes a couple of minor theme/style issues. 	Mitigates a strange Firefox bug that caused weird page loads when closing the location area.

--- bmlt-drupal-satellite-plugin.php
+++ bmlt-drupal-satellite-plugin.php
@@ -3,7 +3,7 @@
 *   \file   bmlt-drupal-satellite-plugin.php                                                *
 *                                                                                           *
 *   \brief  This is a Drupal plugin of a BMLT satellite client.                             *
-*   \version 2.1.20                                                                          *
+*   \version 2.1.21                                                                          *
 *                                                                                           *
     This file is part of the Basic Meeting List Toolbox (BMLT).
     
