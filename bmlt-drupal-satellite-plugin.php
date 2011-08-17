b1f3608ef55aa7ef78921fae2878af7226dfe1e5
Chris M <webservant@magnaws.org>
2011-08-17 10:15:24 -0400

    2.1.23 Release     Improved some of the basic styles in the new map search info windows.

--- bmlt-drupal-satellite-plugin.php
+++ bmlt-drupal-satellite-plugin.php
@@ -3,7 +3,7 @@
 *   \file   bmlt-drupal-satellite-plugin.php                                                *
 *                                                                                           *
 *   \brief  This is a Drupal plugin of a BMLT satellite client.                             *
-*   \version 2.1.22                                                                         *
+*   \version 2.1.23                                                                         *
 *                                                                                           *
     This file is part of the Basic Meeting List Toolbox (BMLT).
     
