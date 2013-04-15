14841fe5adf077f0a72519dc61b6826a01a0062a
chris@littlegreenviper.com <webservant@magnaws.org>
2013-04-15 13:30:40 -0400

    Upped the version number

--- bmlt-drupal-satellite-plugin.php
+++ bmlt-drupal-satellite-plugin.php
@@ -3,7 +3,7 @@
 *   \file   bmlt-drupal-satellite-plugin.php                                                *
 *                                                                                           *
 *   \brief  This is a Drupal plugin of a BMLT satellite client.                             *
-*   \version 3.0.3                                                                          *
+*   \version 3.0.4                                                                          *
 *                                                                                           *
     This file is part of the Basic Meeting List Toolbox (BMLT).
     
