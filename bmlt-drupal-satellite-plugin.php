2c9f9aa58bd199a1cd11798fba936e1958a37d4d
chris@littlegreenviper.com <webservant@magnaws.org>
2013-04-21 15:01:21 -0400

    3.0.7 Release. Fixed a localization bug with the string search.

--- bmlt-drupal-satellite-plugin.php
+++ bmlt-drupal-satellite-plugin.php
@@ -3,7 +3,7 @@
 *   \file   bmlt-drupal-satellite-plugin.php                                                *
 *                                                                                           *
 *   \brief  This is a Drupal plugin of a BMLT satellite client.                             *
-*   \version 3.0.5                                                                          *
+*   \version 3.0.7                                                                          *
 *                                                                                           *
     This file is part of the Basic Meeting List Toolbox (BMLT).
     
