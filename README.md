a00408bc4a9aeb96891d4f827e9cb625a7628a80
pjaudiomv <pjaudiomv@gmail.com>
2018-12-20 08:10:57 -0500

    ver bump 3.9.5

--- README.md
+++ README.md
@@ -28,6 +28,13 @@ This module operates through the use of text filters/input formats. You need to 
 
 CHANGELIST
 ----------
+
+***Version 3.9.5* ** *- December 20, 2018*
+
+- Fix for the BMLT_CHANGES shortcode.
+- WML 1.1 fix for BMLT_MOBILE shortcode.
+- ROOTPATH must ALWAYS be defined, but if its not we must account for that properly.
+
 ***Version 3.9.4* ** *- UNRELEASED*
 
 - Migrated to use composer instead of submodules.
--- bmlt-drupal-satellite-plugin.php
+++ bmlt-drupal-satellite-plugin.php
@@ -3,7 +3,7 @@
 *   \file   bmlt-drupal-satellite-plugin.php                                                *
 *                                                                                           *
 *   \brief  This is a Drupal plugin of a BMLT satellite client.                             *
-*   \version 3.9.4                                                                          *
+*   \version 3.9.5                                                                          *
 *                                                                                           *
     This file is part of the Basic Meeting List Toolbox (BMLT).
 
